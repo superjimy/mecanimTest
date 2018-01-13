2ca138b0a92c88c5403c71537e32620296646879
superjimy <tadashi.azegami@gmail.com>
2018-01-13 16:04:31 +0900

    mecanimTest
    mecanimを使ってUnityちゃんを動かそう

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//マウスがクリックされた場合
+		if (Input.GetMouseButtonDown (0)) {
+			//Animatiorコンポーネントを取得し、"jumpTrigger"をtrueにする
+			GetComponent<Animator> ().SetTrigger ("jumpTrigger");
+		}
+	}
+}
